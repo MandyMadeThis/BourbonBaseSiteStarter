7d38b5238a8d16d3d3ffea607ddb99f6ff0370f1
Mandy <hi@mandymadethis.com>
2016-05-19 17:39:43 -0400

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,93 +3,161 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        // concatenate scripts - for development only. **Don't forget to add scripts array in order of dependencies
+        // concatenate scripts - **Don't forget to add scripts array in order of dependencies
         concat: {
             options: {
-                separator: ';',
+                separator: ";\n",
+                stripBanners: true,
+                sourceMap: true,
             },
-            dist: {
-                src: ['js//scripts.js'],
-                dest: 'js/scripts.min.js'
+            our_scripts: {
+                // First concatenate all our modular scripts and make them one source.js file
+                src: ["js/source/**/!(init)*.js", "js/source/init.js"],
+                dest: "js/build/scripts.js",
+            },
+            // Concatenate all the vendor scripts and since these are already minified, we'll just send them straight to a .min file
+            vendor_scripts: {
+                src: ["js/vendor/**/*.js"],
+                dest: "js/plugins.min.js",
             },
         },
 
-        // concatenate and minify scripts - prod only. **Don't forget to add scripts array in order of dependencies
+        // Minify the script file that concat spit out. 
+        //**Don"t forget to add scripts array in order of dependencies
         uglify: {
-            build: {
-                src: ['js/scripts.js'],
-                dest: 'js/scripts.min.js'
+            options: {
+                preserveComments: "some",
+                mangle: false,
+                sourceMap: true,
+                sourceMapIn: "js/build/scripts.js.map"
+            },
+            my_target: {
+                files: {
+                    "js/scripts.min.js": ["js/build/scripts.js"],
+                }
             }
         },
 
-        // compile Sass.  Expanded for dev and compressed for prod.
+        // compile Sass - with standard sourcemaps
         sass: {
-            dev: {
-                options: {
-                    style: 'expand',
-                    lineNumbers: true
-                },
-                files: {
-                    'css/style.css': 'sass/style.scss'
-                }
-            },
             prod: {
                 options: {
-                    style: 'compressed',
+                    style: "compressed",
+                    sourcemap: "file"
                 },
+
                 files: {
-                    'css/style.css': 'sass/style.scss'
+                    "css/style.css": "scss/style.scss"
                 }
+            },
+
+            dev: {
+                options: {
+                    style: "expanded",
+                    sourcemap: "file"
+                },
+
+                files: {
+                    "css/style.css": "scss/style.scss"
+                }
+            }
+        },
+
+        /// Auto prefix our CSS 
+        postcss: {
+            options: {
+                map: {
+                    prev: false,
+                    inline: false, // save all sourcemaps as separate files... 
+                    annotation: "css/" // ...to the specified directory 
+                },
+
+                processors: [
+                    // add vendor prefixes 
+                    require("autoprefixer")({
+                        browsers: [
+                            "last 2 versions",
+                            "> 3% in US",
+                            "> 3% in CA",
+                            "ie >= 10"
+                        ]
+                    }),
+                    // combine our media queries
+                    require("css-mqpacker")({
+                        sort: true
+                    })
+                ]
+            },
+            dist: {
+                src: "css/*.css"
             }
         },
 
         // watch these files and do these tasks when something changes
         watch: {
-            options: {
-                livereload: true
-            },
-
             scripts: {
-                files: ['js/*.js'],
-                tasks: ['concat'],
-                options: {
-                    spawn: false,
-                },
+                files: ["js/**/*.js"],
+                tasks: ["concat:our_scripts", "uglify"]
             },
-
             css: {
-                files: ['sass/*.scss'],
-                tasks: ['sass:dev'],
+                files: ["scss/**/*.scss"],
+                tasks: ["sass:dev", "postcss"]
+            },
+            html: {
+                files: ["**/*.html"],
                 options: {
-                    spawn: false,
+                    reload: true
+                }
+            },
+            configFile: {
+                files: [ "Gruntfile.js"],
+                options: {
+                    reload: true
                 }
             }
         },
 
-        //prefix anything that needs prefixing for the last 3 browser versions - production only
-        autoprefixer: {
-            options: {
-                browsers: ['last 3 version']
-            },
-            your_target: {
-                src: 'css/style.css',
-                dest: 'css/style.css',
-            },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/style.css', 
+                        'js/scripts.min.js', 
+                        '**/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    proxy: 'http://mandy.dev/'
+                }
+            }
         },
 
+        // Notify us only when there's a problem
+        notify_hooks: {
+            options: {
+                enabled: true,
+                success: false, // whether successful grunt executions should be notified automatically
+                duration: 2.5, // the duration of notification in seconds, for `notify-send only
+                title: "OH SNAP!! Looks like there's an issue:", 
+                message: "Houston, we have a problem..."
+            }
+        }
     });
 
     // Grunt  plug-in list.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks("grunt-contrib-concat"); 
+    grunt.loadNpmTasks("grunt-contrib-sass"); 
+    grunt.loadNpmTasks("grunt-contrib-uglify"); 
+    grunt.loadNpmTasks("grunt-contrib-watch"); 
+    grunt.loadNpmTasks("grunt-notify");
+    grunt.loadNpmTasks("grunt-postcss"); 
 
-    // type "grunt" into the terminal for development.
-    grunt.registerTask('default', ['sass:dev', 'concat', 'watch']);
+    //'grunt' for development tasks.
+    grunt.registerTask("default", ["concat:our_scripts", "uglify", "sass:dev", "postcss", "notify_hooks", 'browserSync', "watch"]);
 
-    // type "grunt prod"  into the terminal for production .
-    grunt.registerTask('prod', ['sass:prod', 'autoprefixer', 'uglify']);
+    //'grunt build' for production tasks.
+    grunt.registerTask("build", ["concat", "uglify","sass:prod", "postcss"]);
 
 };
